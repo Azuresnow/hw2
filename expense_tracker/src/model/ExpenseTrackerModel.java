97cdc6f6886327d0016f7e6eb3cde39a2a386b3b
Azuresnow <31681010+Azuresnow@users.noreply.github.com>
2025-04-10 18:10:53 -0400

    encapsulation

--- expense_tracker/src/model/ExpenseTrackerModel.java
+++ expense_tracker/src/model/ExpenseTrackerModel.java
@@ -1,14 +1,14 @@
 package model;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 public class ExpenseTrackerModel {
 
-  public List<Transaction> transactions;
-  public List<Transaction> filteredTransactions;
+  private List<Transaction> transactions;
+  private final List<Transaction> filteredTransactions;
 
   public ExpenseTrackerModel() {
     transactions = new ArrayList<>(); 
@@ -25,19 +25,19 @@ public class ExpenseTrackerModel {
   }
 
   public List<Transaction> getTransactions() {
-    return filteredTransactions.isEmpty() ? transactions : filteredTransactions;
+    return Collections.unmodifiableList(filteredTransactions.isEmpty() ? transactions : filteredTransactions);
   }
 
   public List<Transaction> getFiltedTransactions(double amount, String category) {
-    return transactions.stream()
+    return Collections.unmodifiableList(transactions.stream()
             .filter(e -> amount == 0 || e.getAmount() == amount) 
             .filter(e -> category.isEmpty() || e.getCategory().toLowerCase().contains(category.toLowerCase())) 
-            .collect(Collectors.toList());
+            .collect(Collectors.toList()));
  }
 
 
   public List<Transaction> getAllTransactions() {
-  return transactions;
+    return Collections.unmodifiableList(transactions);
   }
 
  
